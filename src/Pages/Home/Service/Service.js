222a9d28463f5d528cc684dc876a1be8a881e4c0
Hasan75 <hasanjab14@gmail.com>
2021-10-21 01:10:52 +0600

    services being responsive

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -7,7 +7,7 @@ const Service = ({service}) => {
     return (
         <div className="service">
             <img src={img} alt="" srcset="" />
-            <h2>{name}</h2>
+            <h3>{name}</h3>
             <h5>Price: {price}</h5>
             <p>{description}</p>
         </div>
