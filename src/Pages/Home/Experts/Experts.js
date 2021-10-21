7c872991595cde3da8567de7ed7e120db13d440a
Hasan75 <hasanjab14@gmail.com>
2021-10-22 00:44:33 +0600

    experts added and bootstrap added

---
+++ src/Pages/Home/Experts/Experts.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Expert from '../Expert/Expert'
+
+import mechanic1 from '../../../images/mechanic/mechanic-1.jpg';
+import mechanic2 from '../../../images/mechanic/mechanic-2.jpg';
+import mechanic3 from '../../../images/mechanic/mechanic-3.jpg';
+import mechanic4 from '../../../images/mechanic/mechanic-4.jpg';
+
+const experts = [
+    {
+        img: mechanic1,
+        name: 'Andrew Smith',
+        expertize: '-Engine Expert-'
+    },
+    {
+        img: mechanic2,
+        name: 'John Aderson',
+        expertize: '-Polish Expert-'
+    },
+    {
+        img: mechanic3,
+        name: 'Zakaria Smith',
+        expertize: '-Coloring Expert-'
+    },
+    {
+        img: mechanic4,
+        name: 'Sakib Smith',
+        expertize: '-Alrounder Expert-'
+    },
+]
+const Experts = () => {
+    return (
+        <div className="container">
+            <h2 className="text-primary">Our Experts</h2>
+            <div className="row">
+                {
+                    experts.map(expert => <Expert expert={expert}></Expert>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Experts;<h2>Experts</h2>
