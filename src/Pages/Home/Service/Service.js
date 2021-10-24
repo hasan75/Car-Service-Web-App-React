44df5e16c745fc2730d29959f03aea303eef1bea
Hasan75 <hasanjab14@gmail.com>
2021-10-24 15:10:46 +0600

    button added

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -5,11 +5,12 @@ const Service = ({service}) => {
     // const {service} = props;
      const {name, price, description, img} = service;
     return (
-        <div className="service">
+        <div className="service pb-3">
             <img src={img} alt="" srcset="" />
             <h3>{name}</h3>
             <h5>Price: {price}</h5>
             <p className="px-3">{description}</p>
+            <button className="btn btn-warning">Book {name.toLowerCase()}</button>
         </div>
     );
 };
