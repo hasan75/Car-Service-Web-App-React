8db284f6c0cc4df913b407d397aa8e7fb63f0aa3
Hasan75 <hasanjab14@gmail.com>
2021-10-24 16:41:02 +0600

    Nav Link as={Link} added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -9,12 +10,12 @@ const Header = () => {
                     <Navbar.Brand href="#home">Car Service</Navbar.Brand>
                     <Navbar.Toggle />
                     <Navbar.Collapse className="justify-content-end">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link  href="#services">Services</Nav.Link>
-                        <Nav.Link  href="#experts">Experts</Nav.Link>
-                        <Nav.Link href="#pricing">Login</Nav.Link>
+                        <Nav.Link as={Link} to="/home#home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/home#services">Services</Nav.Link>
+                        <Nav.Link as={Link} to="/home#experts">Experts</Nav.Link>
+                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
                         <Navbar.Text>
-                            Signed in as: <a href="#login">Mark Otto</a>
+                            Signed in as: <a href="#login">Kuddus</a>
                         </Navbar.Text>
                     </Navbar.Collapse> 
                 </Container>
