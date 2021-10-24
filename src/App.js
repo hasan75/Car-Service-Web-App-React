abb29df4acc730d2aaac3cfa052bf4d441ff3d0f
Hasan75 <hasanjab14@gmail.com>
2021-10-24 15:16:13 +0600

    dynamic route set upped and worked

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './Pages/Home/Home/Home';
 import NotFound from './Pages/NotFound/NotFound';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Booking from './Pages/Booking/Booking/Booking';
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
             </Route>
             <Route exact path='/home'>
               <Home></Home>
+            </Route>
+            <Route exact path='/booking/:serviceId'>
+              <Booking></Booking>
             </Route>
             <Route path='*'>
               <NotFound></NotFound>
